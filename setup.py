9fe00e5742a6d5438ee3d00447541fd414336ac9
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2024-10-25 19:01:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="midone",
-    version="1.0.3",
+    version="1.0.4",
     description="Online autonomous time series prediction of near martingales",
     long_description=README,
     long_description_content_type="text/markdown",
